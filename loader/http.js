a4378f377e461b5e68af409c0bdccc502b34e2fb
Hai Phan <hai.phan@gmail.com>
2017-07-25 12:58:32 -0700

    add BBC, Reuters, AP, Al Jazeera, and RT

--- loader/http.js
+++ loader/http.js
@@ -10,7 +10,7 @@ exports.load = function(url, cookieJar) {
       gzip: true,
       forever: true,
       headers: {
-        Accept: "text/html,application/xhtml+xml",
+        Accept: "text/html,text/xml,application/xhtml+xml",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
       }
     },
--- util/config.js
+++ util/config.js
@@ -18,6 +18,83 @@ module.exports = {
     },
     {
       "name": "BBC",
+      "lang": "en",
+      "topics": [
+        { "name": "Top Stories", "link": "http://feeds.bbci.co.uk/news/rss.xml" },
+        { "name": "World", "link": "http://feeds.bbci.co.uk/news/world/rss.xml" },
+        { "name": "UK", "link": "http://feeds.bbci.co.uk/news/uk/rss.xml" },
+        { "name": "Business", "link": "http://feeds.bbci.co.uk/news/business/rss.xml" },
+        { "name": "Politics", "link": "http://feeds.bbci.co.uk/news/politics/rss.xml" },
+        { "name": "Health", "link": "http://feeds.bbci.co.uk/news/health/rss.xml" },
+        { "name": "Education & Family", "link": "http://feeds.bbci.co.uk/news/education/rss.xml" },
+        { "name": "Science & Environment", "link": "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" },
+        { "name": "Technology", "link": "http://feeds.bbci.co.uk/news/technology/rss.xml" },
+        { "name": "Entertainment & Arts", "link": "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml" }
+      ]
+    },
+    {
+      "name": "Reuters",
+      "lang": "en",
+      "topics": [
+        { "name": "Arts & Culture", "link": "http://feeds.reuters.com/news/artsculture" },
+        { "name": "Business", "link": "http://feeds.reuters.com/reuters/businessNews" },
+        { "name": "Company News", "link": "http://feeds.reuters.com/reuters/companyNews" },
+        { "name": "Entertainment", "link": "http://feeds.reuters.com/reuters/entertainment" },
+        { "name": "Environment", "link": "http://feeds.reuters.com/reuters/environment" },
+        { "name": "Health News", "link": "http://feeds.reuters.com/reuters/healthNews" },
+        { "name": "Lifestyle", "link": "http://feeds.reuters.com/reuters/lifestyle" },
+        { "name": "Money", "link": "http://feeds.reuters.com/news/wealth" },
+        { "name": "Most Read Articles", "link": "http://feeds.reuters.com/reuters/MostRead" },
+        { "name": "Oddly Enough", "link": "http://feeds.reuters.com/reuters/oddlyEnoughNews" },
+        { "name": "People", "link": "http://feeds.reuters.com/reuters/peopleNews" },
+        { "name": "Politics", "link": "http://feeds.reuters.com/Reuters/PoliticsNews" },
+        { "name": "Science", "link": "http://feeds.reuters.com/reuters/scienceNews" },
+        { "name": "Sports", "link": "http://feeds.reuters.com/reuters/sportsNews" },
+        { "name": "Technology", "link": "http://feeds.reuters.com/reuters/technologyNews" },
+        { "name": "Top News", "link": "http://feeds.reuters.com/reuters/topNews" },
+        { "name": "US News", "link": "http://feeds.reuters.com/Reuters/domesticNews" },
+        { "name": "World", "link": "http://feeds.reuters.com/Reuters/worldNews" }
+      ]
+    },
+    {
+      "name": "Associated Press",
+      "lang": "en",
+      "topics": [
+        { "name": "Top News", "link": "http://hosted.ap.org/lineups/TOPHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "U.S. National", "link": "http://hosted.ap.org/lineups/USHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "World", "link": "http://hosted.ap.org/lineups/WORLDHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Politics", "link": "http://hosted.ap.org/lineups/POLITICSHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Washington", "link": "http://hosted.ap.org/lineups/WASHINGTONHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Business", "link": "http://hosted.ap.org/lineups/BUSINESSHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Technology", "link": "http://hosted.ap.org/lineups/TECHHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Sports", "link": "http://hosted.ap.org/lineups/SPORTSHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Entertainment", "link": "http://hosted.ap.org/lineups/ENTERTAINMENT.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Health", "link": "http://hosted.ap.org/lineups/HEALTHHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Science", "link": "http://hosted.ap.org/lineups/SCIENCEHEADS.rss?SITE=AP&SECTION=HOME" },
+        { "name": "Strange", "link": "http://hosted.ap.org/lineups/STRANGEHEADS.rss?SITE=AP&SECTION=HOME" }
+      ]
+    },
+    {
+      "name": "Al Jazeera",
+      "lang": "en",
+      "topics": [
+        { "name": "All Topics", "link": "http://www.aljazeera.com/xml/rss/all.xml" }
+      ]
+    },
+    {
+      "name": "Russia Today",
+      "lang": "en",
+      "topics": [
+        { "name": "Top News", "link": "https://www.rt.com/rss/news/" },
+        { "name": "Russian Politics", "link": "https://www.rt.com/rss/politics/" },
+        { "name": "USA", "link": "https://www.rt.com/rss/usa/" },
+        { "name": "UK", "link": "https://www.rt.com/rss/uk/" },
+        { "name": "Business", "link": "https://www.rt.com/rss/business/" },
+        { "name": "Sports", "link": "https://www.rt.com/rss/sport/" }
+      ]
+    },
+    {
+      "name": "BBC",
       "lang": "vi",
       "topics": [
         { "name": "Tin Việt Nam", "link": "http://feeds.bbci.co.uk/vietnamese/rss.xml" },
