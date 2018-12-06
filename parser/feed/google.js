116c2063ecbd41359d31878eec1cde5894e5ddee
Hai Phan <hai.phan@gmail.com>
2018-12-06 11:39:52 -0800

    fix Google News parser

--- parser/feed/google.js
+++ parser/feed/google.js
@@ -17,32 +17,23 @@ function toArticle($) {
   return {
     source: title.slice(titleEnd + 3),
     title: title.slice(0, titleEnd),
-    link: getLink(link),
+    link: link,
     relatedArticles: desc.relatedArticles
   };
-}
-
-function getLink(text) {
-  return require("url").parse(text, true).query.url;
 }
 
 function parseDesc(html) {
   var $ = cheerio.load(html);
   return {
-    relatedArticles: $("div.lh > font").get().filter(elem => isRelatedArticle.call(elem, $)).map(elem => toRelatedArticle.call(elem, $))
+    relatedArticles: $("li").get().slice(1,-1).map(elem => toRelatedArticle.call(elem, $))
   };
-}
-
-function isRelatedArticle($) {
-  var children = $(this).children();
-  return children.length == 2 && children.eq(0).is("a") && children.eq(1).is("font");
 }
 
 function toRelatedArticle($) {
   var link = $(this).children("a").first();
   return {
     title: link.text(),
-    link: getLink(link.attr("href")),
+    link: link.attr("href"),
     source: $(this).children("font").first().text()
   };
 }
